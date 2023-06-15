a9d802c25325bd68cec2a9c313ccead07d779617
SteampunkFernando <fernando.quezado@steampunk.com>
2023-06-15 15:37:39 -0400

    changed main.tf file in order to connect to my own instance

--- main.tf
+++ main.tf
@@ -14,19 +14,19 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0d86c69530d0a048e"
+  ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
-  subnet_id = "subnet-02a9c3397a0a8b78b"
-  key_name ="testingTerraform"
+  subnet_id = "subnet-0ec96671454815143"
+  key_name ="PrivateKey6_15_2023"
 
   tags = {
-    Name = "TestInstance for terraform"
+    Name = "Testing Terraform Part 3"
     CreatorName ="fquezado"
   }
 }
 
 output "instance_ip" {
-    value = aws_instance.app_server.public_ip
+    value = aws_instance.app_server.public_dns
     description = "The public IP of the instance"
   }
 
