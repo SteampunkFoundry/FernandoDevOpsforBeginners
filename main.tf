330c3fba01641d89d769e7de79e5432831af9c91
SteampunkFernando <fernando.quezado@steampunk.com>
2023-06-15 12:21:50 -0400

    changed main.tf file in order to connect to my own instance

--- main.tf
+++ main.tf
@@ -17,9 +17,15 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0d86c69530d0a048e"
   instance_type = "t2.micro"
   subnet_id = "subnet-02a9c3397a0a8b78b"
+  key_name ="<testingTerraform>"
 
   tags = {
     Name = "<TestInstance2"
     CreatorName ="<fquezado>"
   }
 }
+
+output "instance_ip" {
+    value = aws_instance.app_server.public_ip
+    description = "The public IP of the instance"
+  }
