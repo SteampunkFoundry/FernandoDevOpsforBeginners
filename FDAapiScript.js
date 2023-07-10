89f25fe3403a156ff5bd83b27464238a4c64a43f
SteampunkFernando <fernando.quezado@steampunk.com>
2023-07-10 14:24:10 -0400

    added reason for recall in the javascript file!

--- FDAapiScript.js
+++ FDAapiScript.js
@@ -2,33 +2,32 @@
 async function getReports() {
     try {
     // Get the zip code from the input field
-    let zipCode = document.getElementById("zipCode").value;
+        let zipCode = document.getElementById("zipCode").value;
 
     // Define your API endpoint with the zip code as a query parameter
-    let url = `https://api.fda.gov/food/enforcement.json?search=postal_code:${zipCode}&limit=5`;
+        let url = `https://api.fda.gov/food/enforcement.json?search=postal_code:${zipCode}&limit=5`;
 
     // Fetch the data from the API
-    let response = await fetch(url);
+        let response = await fetch(url);
 
     // If the response status is not ok, throw an error
-    if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-}
+        if (!response.ok) {
+            new Error(`HTTP error! status: ${response.status}`);
+        }
 
     // Parse the JSON data
-    let data = await response.json();
+        let data = await response.json();
 
     // Clear previous results
-    document.getElementById("result").innerHTML = "";
+        document.getElementById("result").innerHTML = "";
 
     // Display the data in the "result" div
-    data.results.forEach((item, i) => {
-    document.getElementById("result").innerHTML += `
-                <p><strong>Report ${i+1}:</strong></p>
-                <p>Product Description: ${item.product_description}</p>
-                <p>Reason for Recall: ${item.reason_for_recall}</p>
-                <hr>
-            `;
+        data.results.forEach((item, i) => {
+        document.getElementById("result").innerHTML += `
+                    <p><strong>Report ${i+1}:</strong></p>
+                    <p>Product Description: ${item.product_description}</p>
+                    <p>Reason for Recall: ${item.reason_for_recall}</p>
+                    <hr>`;
         });
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
