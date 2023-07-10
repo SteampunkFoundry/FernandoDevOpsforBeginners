0ad9b841cfc8b3412c0063d48f121b67810cf123
SteampunkFernando <fernando.quezado@steampunk.com>
2023-07-10 14:30:37 -0400

    rolling back

--- FDAapiScript.js
+++ FDAapiScript.js
@@ -1,36 +1,36 @@
-
 async function getReports() {
-    try {
     // Get the zip code from the input field
-        let zipCode = document.getElementById("zipCode").value;
+    let zipCode = document.getElementById("zipCode").value;
+
+    // Define a regex for 5 digit ZIP code
+    let zipRegex = /^[0-9]{5}$/;
+
+    // Check if the input matches the 5 digit ZIP code format
+    if (!zipRegex.test(zipCode)) {
+        alert("Please enter a valid 5 digit ZIP code.");
+        return;
+    }
 
     // Define your API endpoint with the zip code as a query parameter
-        let url = `https://api.fda.gov/food/enforcement.json?search=postal_code:${zipCode}&limit=5`;
+    let url = `https://api.fda.gov/food/enforcement.json?search=postal_code:${zipCode}&limit=5`;
 
     // Fetch the data from the API
-        let response = await fetch(url);
+    let response = await fetch(url);
 
-    // If the response status is not ok, throw an error
-        if (!response.ok) {
-            new Error(`HTTP error! status: ${response.status}`);
-        }
-
-    // Parse the JSON data
+    // Check the response status
+    if (!response.ok) {
+        // If the response status is not ok, throw an error
+        throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+        // If the response status is ok, parse the JSON data
         let data = await response.json();
 
-    // Clear previous results
+        // Clear previous results
         document.getElementById("result").innerHTML = "";
 
-    // Display the data in the "result" div
+        // Display the data in the "result" div
         data.results.forEach((item, i) => {
-        document.getElementById("result").innerHTML += `
-                    <p><strong>Report ${i+1}:</strong></p>
-                    <p>Product Description: ${item.product_description}</p>
-                    <p>Reason for Recall: ${item.reason_for_recall}</p>
-                    <hr>`;
+            document.getElementById("result").innerHTML += `<p>Report ${i+1}: ${item.product_description}</p>`;
         });
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
     }
 }
-
